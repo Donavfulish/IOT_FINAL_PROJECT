7c742ae9512b1a7ca660269c6f643861219e9b83
minhluan <luanthuco11@gmail.com>
2025-11-24 17:29:35 +0700

    change database

--- server/config/db.js
+++ server/config/db.js
@@ -1,23 +1,13 @@
+import pkg from "pg";
+const { Pool } = pkg;
 import dotenv from "dotenv";
 dotenv.config();
-
-import { Pool } from "pg";
-
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
-  options: '-c search_path="$user",extensions',
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-console.log(
-  process.env.DB_HOST,
-  process.env.DB_PORT,
-  process.env.DB_NAME,
-  process.env.DB_USER
-);
 (async () => {
   try {
     const result = await pool.query("SELECT NOW()");
@@ -26,5 +16,4 @@ console.log(
     console.error("DB connection failed:", err.message);
   }
 })();
-
 export default pool;
